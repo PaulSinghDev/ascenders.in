7a737c93ef4bd19f7574e6e0e3611e9123c42409
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-01-03 19:32:32 +0000

    Implement serv side rendering for styled components [1 - 10] -- Import dependencies [13 - 46] -- Add a static method to get our props and render our server side style sheet from styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,49 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { AppPropsType, AppType } from "next/dist/shared/lib/utils";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 class PoaApp extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    // Make style sheet
+    const sheet = new ServerStyleSheet();
+    // Get the original page
+    const originalPage = ctx.renderPage;
+
+    try {
+      // Get the original Props
+      const initialProps = await Document.getInitialProps(ctx);
+      // Change the render page method to also collect our stylesheet
+      ctx.renderPage = () =>
+        originalPage({
+          enhanceApp: (App: AppType) => (props: AppPropsType) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      // Return the updated element list
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } catch (e) {
+      console.log(e);
+      return originalPage();
+    } finally {
+      sheet.seal();
+    }
+  }
   render() {
     return (
       <Html lang="en">
