c540e30e950b80c5fda85486c7dad742e9cea592
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-01-05 17:28:37 +0000

    Remove non-existent imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Ascenders | H.E.A.L</title>
         <meta name="description" content="Ascenders | H.E.A.L" />
