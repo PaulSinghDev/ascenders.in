5768b2bc520afa7058f1fe108dfcff83ccaa0767
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-01-05 17:27:07 +0000

    implement global styles [3] -- Import it [7 - 10] -- Implement the component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { GlobalStyles } from "styles";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default MyApp;
