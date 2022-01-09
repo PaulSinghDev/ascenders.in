1a49c142b0eaedf10e40fe2c08915af150061b62
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-01-09 10:58:08 +0000

    Config folder
    This folder will be the place to store all non-sensitive data which is used for configuration of the app. Things like social networks etc will be placed here until we move the CMS/API driven variant of the app

---
+++ config/constants/socialData.ts
@@ -0,0 +1,19 @@
+import { SocialData } from "types";
+
+export const socialData: SocialData = {
+  twitter: {
+    url: "https://twitter.com/ascenders_in",
+    label: "@ascenders_in",
+    network: "twitter",
+  },
+  facebook: {
+    url: "https://facebook.com/ascenders",
+    label: "@ascenders",
+    network: "facebook",
+  },
+  instagram: {
+    url: "https://instagram.com/@ascenders.in",
+    label: "@ascenders.in",
+    network: "instagram",
+  },
+};
