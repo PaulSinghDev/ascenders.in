8a9439c4ccf5dc01b1495b2dce0e3423c571aa4d
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-01-09 10:53:51 +0000

    Rename to types.ts

--- types.d.ts
+++
@@ -1,6 +0,0 @@
-export interface LocalStorageInterface {
-  get: (key: string) => string | null;
-  set: (key: string, value: string) => string | null;
-  remove: (key: string) => void;
-  clear: () => void;
-}
