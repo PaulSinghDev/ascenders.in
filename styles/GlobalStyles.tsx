ca42a9b798cca6d5029a3b904a71efdafdc5fe13
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-01-09 11:01:27 +0000

    Add basic styling
    Add default overrides for basic HTML components and root vars for colors

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -1,12 +1,52 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+:root {
+  --dark: #202020;
+  --grey: #707070;
+  --teal: #7C9A92;
+  --blue: #48A3E0;
+  --green: #95AB63;
+  --orange: #F19557;
+  --light-teal: #7C9A92;
+  --off-grey: #C0C9B1;
+  --off-blue: #607D8B;
+  --light: #F5F5F7;
+  --dark-blue: #546E7A;
+  --vh: 1vh;
+}
+
 html,
 body {
   padding: 0;
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+
+a,
+p,
+ul,
+li {
+  margin: 0;
+  padding: 0;
+}
+
+ul {
+  list-style: none;
+}
+
+button {
+  appearance: none;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+body {
+  letter-spacing: 1.5px;
+  line-height: 1.2;
 }
 
 a {
