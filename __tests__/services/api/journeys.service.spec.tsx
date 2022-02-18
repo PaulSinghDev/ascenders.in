6829bb795f6535c228561a04adc67145ed4610ba
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-02-18 09:28:12 +0000

    Add spec files

--- __tests__/services/api/journeys.service.spec.tsx
+++ __tests__/services/api/journeys.service.spec.tsx
@@ -1,10 +1,22 @@
 import { getJourneyById, getJourneyByTitle, getAllJourneys } from "services";
+import DB from "db.json";
+import axios from "axios";
+import { Journey } from "types";
+
+const JOURNEYS: Journey[] = [...DB.journeys] as Journey[];
+
+jest.mock("axios", () => {
+  const actual = jest.requireActual("axios");
+  return { ...actual, get: jest.fn() };
+});
+
+(axios.get as jest.Mock).mockResolvedValue({ data: [...JOURNEYS] });
 
 describe("getJourneyById", () => {
   it("gets a journey record with the correct id", async () => {
-    const journey = await getJourneyById("0");
+    const journey = await getJourneyById(JOURNEYS[0].id);
     expect(journey).not.toBe(null);
-    expect(journey?.id).toBe("0");
+    expect(journey).toEqual(JOURNEYS[0]);
   });
   it("will return null if there is no record", async () => {
     const journey = await getJourneyById("9999999");
@@ -14,9 +26,9 @@ describe("getJourneyById", () => {
 
 describe("getJourneyByTitle", () => {
   it("gets a journey record with the correct title", async () => {
-    const journey = await getJourneyByTitle("Goa Sunset");
+    const journey = await getJourneyByTitle(JOURNEYS[0].title);
     expect(journey).not.toBe(null);
-    expect(journey?.title).toBe("Goa Sunset");
+    expect(journey).toEqual(JOURNEYS[0]);
   });
   it("will return null if there is no record", async () => {
     const journey = await getJourneyById("Doesn't Exist");
@@ -28,6 +40,6 @@ describe("getAllJourneys", () => {
   it("will return an array of journeys", async () => {
     const journeys = await getAllJourneys();
     expect(typeof journeys).toBe("object");
-    expect(journeys?.length).toBeGreaterThan(0);
+    expect(journeys).toEqual(JOURNEYS);
   });
 });
