71fb47574937210366446629b19f28ed48cf6bca
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2022-02-18 09:29:02 +0000

    Add interests service

---
+++ services/api/interests.service.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { Interest } from "types";
+const API_BASE_URL: string = "http://localhost:3000";
+
+export const getAllInterests = async (): Promise<Interest[] | null> => {
+  try {
+    const res = (await axios.get(`${API_BASE_URL}/interests`))
+      .data as Interest[];
+    return res ? res : null;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+export const getInterestById = async (id: string): Promise<Interest | null> => {
+  try {
+    const res = (await axios.get(`${API_BASE_URL}/interests`))
+      .data as Interest[];
+    const interest = res.find((i) => i.id === id);
+    return interest ? interest : null;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+export const getInterestByName = async (
+  name: string
+): Promise<Interest | null> => {
+  try {
+    const res = (await axios.get(`${API_BASE_URL}/interests`))
+      .data as Interest[];
+    const interest = res.find((i) => i.name === name);
+    return interest ? interest : null;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
