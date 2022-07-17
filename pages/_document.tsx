a371e00a519ab4b999e092fe9e76a26f0858113d
Paul Singh <im.makingstuffs@gmail.com>
2022-07-17 09:47:41 +0100

    Fix: Correct the issue with server style sheet not being rendered properly

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,52 +9,25 @@ import Document, {
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
-class PoaApp extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    // Make style sheet
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
-    // Get the original page
-    const originalPage = ctx.renderPage;
+    const originalRenderPage = ctx.renderPage;
 
     try {
-      // Get the original Props
-      const initialProps = await Document.getInitialProps(ctx);
-      // Change the render page method to also collect our stylesheet
       ctx.renderPage = () =>
-        originalPage({
-          enhanceApp: (App: AppType) => (props: AppPropsType) =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
         });
-      // Return the updated element list
+
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
+        styles: [initialProps.styles, sheet.getStyleElement()],
       };
-    } catch (e) {
-      console.log(e);
-      return originalPage();
     } finally {
       sheet.seal();
     }
   }
-  render() {
-    return (
-      <Html lang="en">
-        <Head></Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
 }
-
-export default PoaApp;
