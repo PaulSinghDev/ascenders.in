4451c08bb906ac5f3e89d763e1848eb17ce47b2a
Paul Singh <im.makingstuffs@gmail.com>
2022-07-31 07:42:49 +0100

    Chore: Update the name of the component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
 import type { AppProps } from "next/app";
 import { GlobalStyles, GlobalFonts } from "styles";
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <GlobalFonts />
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
-  );
-};
+const Ascenders = ({ Component, pageProps }: AppProps) => (
+  <>
+    <GlobalFonts />
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </>
+);
 
-export default MyApp;
+export default Ascenders;
