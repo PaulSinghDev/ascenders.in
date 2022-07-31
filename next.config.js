cf8fa43aa5278608607587dbb16f37070faf0ad3
Paul Singh <im.makingstuffs@gmail.com>
2022-07-31 07:35:30 +0100

    Chore: Add compiler options to include styled components

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+    },
+  },
 };
