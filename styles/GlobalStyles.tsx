dab9ed8aa7955a296d4c90aa383e124b54c0f934
Paul Singh <im.makingstuffs@gmail.com>
2022-10-03 17:10:16 +0100

    Chore: Update to new colour schemes and add in new fonts

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -2,17 +2,19 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
 :root {
-  --dark: #202020;
+  --dark: #262628;
   --grey: #707070;
   --teal: #7C9A92;
   --blue: #48A3E0;
   --green: #95AB63;
-  --orange: #F19557;
+  --dark-green: #568459;
+  --orange: #ed7a2d;
   --light-teal: #7C9A92;
+  --grey: #838383;
   --off-grey: #C0C9B1;
   --off-blue: #607D8B;
   --light: #F5F5F7;
-  --light-grey: #ebebeb;
+  --light-grey: #f2f4f5;
   --dark-blue: #546E7A;
   --vh: 1vh;
   --padding-sm: 0.5rem;
@@ -26,6 +28,7 @@ export const GlobalStyles = createGlobalStyle`
   --border-radius-md: 0.75rem;
   --border-radius-lg: 1rem;
   --border-radius-xl: 2rem;
+  --main-nav-height: 60px;
 }
 
 html,
@@ -43,6 +46,7 @@ li {
   margin: 0;
   padding: 0;
 }
+
 h1,
 h2,
 h3,
@@ -65,7 +69,9 @@ html {
 }
 
 body {
-  line-height: 1.2;
+  line-height: 1.6;
+  letter-spacing: 0.15pt;
+  padding-top: var(--main-nav-height);
 }
 
 a {
@@ -73,7 +79,17 @@ a {
   text-decoration: none;
 }
 
+p {
+  margin-bottom: 12px;
+  &:last-child,
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+}
+
 * {
   box-sizing: inherit;
+  font-family: inherit;
+  line-height: inherit;
 }
 `;
