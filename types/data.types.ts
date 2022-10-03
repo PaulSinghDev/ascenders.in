d9f981ef45b3d14135c33937c6bb21a14c0933d6
Paul Singh <im.makingstuffs@gmail.com>
2022-10-03 17:10:37 +0100

    Addition: Added a variety of types

--- types/data.types.ts
+++ types/data.types.ts
@@ -1,3 +1,10 @@
+export interface BasicNavLink {
+  title: string;
+  href: string;
+  label: string;
+  icon?: AvailableIcons;
+}
+
 export type Interest = {
   slug: string;
   title: string;
@@ -5,7 +12,6 @@ export type Interest = {
   target: string[];
   thumbnail: string;
   gallery?: string[];
-  level: "small" | "medium" | "large";
   id: string;
 };
 
@@ -49,16 +55,147 @@ export type MinMaxType = {
   unit: UnitType;
 };
 
-export interface Journey {
-  id: string;
+export type Meal =
+  | "breakfast"
+  | "lunch"
+  | "brunch"
+  | "dinner"
+  | "supper"
+  | "snacks"
+  | "tea";
+
+export interface ItineraryDay extends AccordionContent {
+  day: number;
+  meals: Meal[];
+}
+
+export interface ItineraryInterface {
+  title: string;
+  description: string[];
+  days: ItineraryDay[];
+}
+
+export interface HealOverview {
+  h: {
+    title: string;
+    copy: string[];
+  };
+  e: {
+    title: string;
+    copy: string[];
+  };
+  a: {
+    title: string;
+    copy: string[];
+  };
+  l: {
+    title: string;
+    copy: string[];
+  };
+}
+
+export type ImageType = {
+  src: string;
+  alt: string;
+  srcset?: string;
+  caption?: string;
+};
+
+export interface Staff {
+  name: string;
+  role: string;
+  image: ImageType;
+  slug: string;
+  alias?: string;
+}
+
+export type Difficulty =
+  | "very easy"
+  | "easy"
+  | "easy"
+  | "medium"
+  | "hard"
+  | "very hard";
+
+export type AccommodationType = "camping" | "hotel" | "homestay";
+export type Accommodation = {
+  type: AccommodationType[];
+};
+
+export type AccordionContent = {
   title: string;
   content: string;
-  price: PriceType;
-  duration: MinMaxType;
-  level: [number, number];
-  locationId: string;
-  interestIds: string[];
-  permalink: string;
-  groupSize: MinMaxType;
-  thumbnail: string;
+  icon?: AvailableIcons;
+};
+
+export interface AccordionSection {
+  title: string;
+  description: string[];
+  items: AccordionContent[];
 }
+
+export interface Journey {
+  title: string;
+  itinerary: ItineraryInterface;
+  interest: string[];
+  tagline: string;
+  staff: Staff[];
+  heal: HealOverview;
+  importantInformation: AccordionSection;
+  duration: {
+    value: number;
+    measurement: "days" | "weeks";
+  };
+  slug: string;
+  type: string;
+  description: string[];
+  overviewBullets: string[];
+  thumbnail: string;
+  inclusions: AccordionSection;
+  gallery: GalleryImage[];
+  level: {
+    min: Difficulty;
+    max?: Difficulty;
+  };
+  id: string;
+  group: {
+    min: number;
+    max: number;
+    measurement: "people" | "couples";
+  };
+  location: {
+    label: string;
+  };
+  accommodation: Accommodation;
+  price: {
+    value: number;
+    currency: "₹" | "$" | "£";
+    from: number;
+    measurement: "person" | "group" | "couple";
+  };
+}
+
+export interface GalleryImage {
+  src: string;
+  srcset?: string[];
+  title: string;
+  interest: string[];
+  alt: string;
+  description: string[];
+}
+
+export type AvailableIcons =
+  | "expect"
+  | "essentials"
+  | "fitness"
+  | "plus"
+  | "minus"
+  | "temperature"
+  | "map"
+  | "safety"
+  | "heal-h"
+  | "heal"
+  | "book"
+  | "home"
+  | "leaf"
+  | "menu";
