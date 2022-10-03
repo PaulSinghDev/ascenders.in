3173cf3e3d480b680629575bd6b655b994a520f5
Paul Singh <im.makingstuffs@gmail.com>
2022-10-03 17:08:19 +0100

    Addtion: Add in the footer and nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
+import { MainNav } from "@/components/MainNav/MainNav";
 import type { AppProps } from "next/app";
 import { GlobalStyles, GlobalFonts } from "styles";
+import { Footer } from "../components";
 
 const Ascenders = ({ Component, pageProps }: AppProps) => (
   <>
     <GlobalFonts />
     <GlobalStyles />
+    <MainNav />
     <Component {...pageProps} />
+    <Footer />
   </>
 );
 
