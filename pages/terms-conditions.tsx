8ee1d5d7d71ac0bd649660f5586e80a6bfb042a2
Paul Singh <im.makingstuffs@gmail.com>
2022-10-21 16:23:08 +0100

    :hammer: Addition: Add a terms page and some data

---
+++ pages/terms-conditions.tsx
@@ -0,0 +1,25 @@
+import { Accordion } from "@/components/Accordion/Accordion";
+import { Header } from "components/Header";
+import Head from "next/head";
+import { terms } from "data/terms";
+
+const Search: React.FC<{}> = () => (
+  <div>
+    <Head>
+      <title>Ascenders | H.E.A.L | Our Journeys | Search</title>
+      <meta
+        name="description"
+        content="Ascenders | H.E.A.L | Ascenders | H.E.A.L | Our Journeys | Search"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Header
+      pageType="info"
+      heading="Terms and Conditions"
+      subheading="Please Read Depending on the individual Journey that the participant chooses, the applicable Terms & Conditions, including but not limited to, cost, deposit, payments, cancellations, and refunds, may differ from those listed below. In pre-Journey communications, a copy of the complete Terms and Policies that apply to each participant will be issued. Any term that is found to be unlawful or unenforceable by a court of competent jurisdiction will not impact the legality or application of any other provision, and will be considered removed from this Agreement."
+    />
+    <Accordion title="" description={[]} items={terms} />
+  </div>
+);
+
+export default Search;
