04fbb59c4b354eb9e3cab7615a71b2d761315369
Paul Singh <im.makingstuffs@gmail.com>
2022-11-15 17:12:31 +0000

    :lipstick: Style: Update the form field colour to be actual white

--- components/BookingForm/BookingForm.tsx
+++ components/BookingForm/BookingForm.tsx
@@ -45,7 +45,7 @@ const FormInput = styled.input`
   border-width: 1px;
   border-radius: var(--border-radius-md);
   border-color: rgba(0, 0, 0, 0.2);
-  background-color: var(--light);
+  background-color: #fff;
 
   &[data-valid="false"] {
     outline: var(--red) auto 1px;
@@ -68,7 +68,7 @@ const FormSelect = styled.select`
   border-radius: var(--border-radius-md);
   border-color: rgba(0, 0, 0, 0.2);
   appearance: none;
-  background-color: var(--light);
+  background-color: #fff;
 
   &:focus-visible {
     outline: -webkit-focus-ring-color auto 1px;
